c7f8e5c8bc10f943eb61efec47070f3c7d10d5e5
copergas <copergas.cpg@gmail.com>
2016-09-02 16:12:53 -0300

    Criada base para tela Postos

---
+++ app/pages/postos/postos.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the PostosPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/postos/postos.html',
+})
+export class PostosPage {
+
+  constructor(private navCtrl: NavController) {
+
+  }
+
+}
