21cecd2e99dece2636dcfdfd5ff04ce50b033946
copergas <copergas.cpg@gmail.com>
2016-09-05 11:18:51 -0300

    Funcao de navegacao do Menu

--- app/app.ts
+++ app/app.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {Platform, ionicBootstrap} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {MapaPage} from './pages/mapa/mapa';
+import {PostosPage} from './pages/postos/postos';
 
 
 @Component({
@@ -9,10 +10,15 @@ import {MapaPage} from './pages/mapa/mapa';
 })
 export class MyApp {
 
-  private rootPage: any;
+  private rootPage: any; //A pagina que será exibida na tela
+  private mapa: any; // Pagina do Mapa
+  private postos: any; // Pagina que lista os Postos
 
   constructor(private platform: Platform) {
-    this.rootPage = MapaPage;
+    this.mapa = MapaPage;
+    this.postos = PostosPage;
+
+    this.rootPage = this.mapa;
 
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
@@ -20,28 +26,12 @@ export class MyApp {
       StatusBar.styleDefault();
     });
   }
+
+  irPara(pagina){
+    this.rootPage = pagina;
+  }
+
+
 }
 
 ionicBootstrap(MyApp);
-// import { Component } from '@angular/core';
-// import { ionicBootstrap, Platform } from 'ionic-angular';
-// import { StatusBar } from 'ionic-native';
-// import { MapaPage } from './pages/mapa/mapa';
-//
-// @Component({
-//   templateUrl: 'build/pages/menu/menu.html'
-// })
-// export class MyApp {
-//   private rootPage: any ;
-//
-//   constructor(public platform: Platform) {
-//     this.rootPage = MapaPage;
-//     platform.ready().then(() => {
-//       // Okay, so the platform is ready and our plugins are available.
-//       // Here you can do any higher level native things you might need.
-//       StatusBar.styleDefault();
-//     });
-//   }
-// }
-//
-// ionicBootstrap(MyApp);
